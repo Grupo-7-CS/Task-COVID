f8bd8b417a4d9291c53224f6cc03bbb67d548263
flavia29 <61882314+flavia29@users.noreply.github.com>
2021-03-28 12:18:45 +0200

    bug

--- app/src/main/java/com/example/task_ovid/StartActivity.java
+++ app/src/main/java/com/example/task_ovid/StartActivity.java
@@ -1,10 +1,12 @@
 package com.example.task_ovid;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 /*
@@ -19,7 +21,9 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-
+        Context context = getApplicationContext();
+        Toast toast = Toast.makeText(context,"BUG", Toast.LENGTH_SHORT);
+        toast.show();
         stbut= (ImageButton) findViewById(R.id.button_start);
         stbut.setOnClickListener(new View.OnClickListener() {
             @Override
