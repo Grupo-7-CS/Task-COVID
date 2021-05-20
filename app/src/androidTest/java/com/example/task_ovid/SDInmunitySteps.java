bedea14e249e916829bef370f1862ed430c70b93
dimonra13 <diegomontoto4@hotmail.com>
2021-05-20 19:11:28 +0200

    T4: Inmunity steps updated

--- app/src/androidTest/java/com/example/task_ovid/SDInmunitySteps.java
+++ app/src/androidTest/java/com/example/task_ovid/SDInmunitySteps.java
@@ -48,11 +48,13 @@ public class SDInmunitySteps {
     @Then("^The user still has 100 health points and 2 immunity points left.$")
     public void user_life_is_still_100_points() throws Throwable {
         assertEquals(100,Vida.getVidaActual());
+        assertEquals(2,Vida.getInmunidadActual());
     }
 
     @Then("^The user's life is reduced to 55 points and he is no longer immune.$")
     public void user_life_is_reduced_to_55_points() throws Throwable {
         assertEquals(55,Vida.getVidaActual());
+        assertEquals(0,Vida.getInmunidadActual());
     }
 
 }
